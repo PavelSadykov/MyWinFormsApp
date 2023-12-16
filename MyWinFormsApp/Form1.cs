c86a4fb707c522a9f7a7050c51f4fbedccf6b364
PavelSadykov <2439747@gmail.com>
2023-12-16 23:49:16 +0500

    Добавьте файлы проекта.

---
+++ MyWinFormsApp/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using StyleLibrary;
+
+namespace MyWinFormsApp
+{
+    public partial class Form1 : Form
+    {
+     
+        public Form1()
+        {
+            InitializeComponent();
+
+            StyleProcessor.ChangeButtonStyle(button1, Color.Red, Color.White, new Font("Arial", 12, FontStyle.Bold));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
---
+++ MyWinFormsApp/SecondForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace MyWinFormsApp
+{
+    public partial class SecondForm : Form
+    {
+        private Button myButton;
+
+        public SecondForm()
+        {
+            InitializeComponent();
+            ModifyButtonStyle();
+        }
+
+        private void ModifyButtonStyle()
+        {
+            Form1 firstForm = new Form1();
+
+            // Ищем кнопку по имени из формы FirstForm
+            if (firstForm.Controls.ContainsKey("myButton"))
+            {
+                myButton = firstForm.Controls["myButton"] as Button;
+
+                if (myButton != null)
+                {
+                    myButton.BackColor = Color.Red;
+                    myButton.ForeColor = Color.White;
+                    myButton.Font = new Font("Arial", 12, FontStyle.Bold);
+                }
+            }
+        }
+    }
+}
